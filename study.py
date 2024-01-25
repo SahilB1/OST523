04253b6f7a319b5aeba3bc4eac19e2affec74e9c
Sahil Bagga <sahilbagga.innovate@gmail.com>
2024-01-24 23:35:49 -0500

    Add files via upload

---
+++ study.py
@@ -0,0 +1,40 @@
+import random
+
+def study_ost():
+    content = open("input.txt", "r")
+    txt = str(content.read())
+    split_txt = txt.split("NEXT")
+    
+    test = {}
+    for line in split_txt:
+        split_line = line.split("\n")[:-1]
+        correct_answers = split_line[1].split(",")[1:]
+        questions = split_line[3:]
+        for i, q in enumerate(questions):
+            complete_question = ""
+            qa = q.split("?")
+            # print(qa)
+            question = qa[0]
+            answers = qa[1].split("*")
+            complete_question += (question + "?\n")
+            for a in answers:
+                complete_question += a + "\n"
+            test[complete_question] = correct_answers[i]
+    
+    keys = list(test.keys())
+    random.shuffle(keys)
+    total_correct = 0
+    for key in keys:
+        print(key)
+        print()
+        user_answer = input("Your answer?\t")
+        if user_answer.lower() == test[key].lower():
+            print("Correct!\n\n")
+            total_correct += 1
+        else:
+            print("Incorrect | Correct Answer:", test[key], "\n\n")
+    print("Grade:", str(total_correct) + "/" + str(len(keys)))
+    print("Percent Correct:", str(int(100 * (total_correct/len(keys)))) + "%")
+            
+if __name__ == "__main__":
+    study_ost()
